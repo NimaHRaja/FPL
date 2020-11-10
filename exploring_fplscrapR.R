6c8d0cb70cb9f424f5704e7109d6fd0e3fafe03b
NimaHRaja <nimaradja@gmail.com>
2020-11-10 16:53:09 +0000

    more exploring

--- exploring_fplscrapR.R
+++ exploring_fplscrapR.R
@@ -2,8 +2,32 @@
 
 library(fplscrapR)
 
-fplscrapR::get_player_hist(111) %>% View()
-fplscrapR::get_entry_hist(222) %>% View()
-fplscrapR::get_entry_picks(333,2)$picks %>% View()
 fplscrapR::get_league(22) %>% View()
 fplscrapR::get_player_id()
+
+
+fplscrapR::get_entry(11111) %>% View()
+fplscrapR::get_entry_captain(1234,4) %>% View()
+fplscrapR::get_entry_hist(5421) %>% View()
+A <- fplscrapR::get_entry_picks(53720,6)
+A$automatic_subs %>% View()
+A$entry_history %>% View()
+A$picks %>% View()
+fplscrapR::get_entry_player_picks(8024,2) %>% View()
+fplscrapR::get_entry_season(83230) %>% View()
+fplscrapR::get_fdr() %>% View()
+fplscrapR::get_game_list() %>% View()
+fplscrapR::get_game_stats(35) %>% View()
+B <- fplscrapR::get_league(897187)
+B$league %>% View()
+B$new_entries %>% View()
+B$standings$results %>% View()
+C <- fplscrapR::get_league_entries(897187)
+D <- fplscrapR::get_player_details()
+fplscrapR::get_player_hist(2) %>% View()
+fplscrapR::get_player_id("Hugo Lloris") %>% View()
+fplscrapR::get_player_info("Hugo Lloris") %>% View()
+fplscrapR::get_player_name(4) %>% View()
+fplscrapR::get_round_info(2) %>% View()
+
+
