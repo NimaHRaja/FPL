491f7b2be952d811c2e683ec12b23c4fe6107c4b
NimaHRaja <nimaradja@gmail.com>
2020-11-24 16:46:02 +0000

    minor update

--- Functions/init.R
+++ Functions/init.R
@@ -4,3 +4,7 @@ library(fplscrapR)
 library(dplyr)
 library(ggplot2)
 library(reshape2)
+
+source("Functions/get_league_entries_updated.R")
+source("Functions/get_league_updated.R")
+source("Functions/get_a_league_teams.R")
