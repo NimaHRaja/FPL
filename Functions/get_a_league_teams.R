58d546a7e105a74481b400b7044ba02992d556a5
NimaHRaja <nimaradja@gmail.com>
2020-11-28 17:22:58 +0000

    search for local added to get_a_league_teams

--- Functions/get_a_league_teams.R
+++ Functions/get_a_league_teams.R
@@ -1,16 +1,34 @@
 # get_a_league_teams recieves league_id and week_number
 # and returns the teams of entries of 
 # the given league_id for the given week_number + metadata.
+# it also searches for a local version of the data file before 
+# fetching the data from FPL.
 
-get_a_league_teams <- function(league_id, week_number){
+get_a_league_teams <- function(league_id, week_number, first_page = 1, num_pages = 1){
     
-    league_entries <- get_league_entries_updated(league_id)
+    output_file <- 
+        paste("data/entries_teams_", league_id, "_week", week_number, "_", 
+              first_page, "-", num_pages, ".csv", sep = "")
     
-    do.call("rbind", lapply(league_entries$entry,
-                            function(x){get_entry_picks(x, week_number)$picks %>% 
-                                    mutate(entry = x)})) %>%
-        inner_join(league_entries, by = "entry") %>%
-        mutate(league_id = league_id, 
-               week_number = week_number,
-               league_name = get_league_updated(1138028)$league$name)
+    if(file.exists(output_file)){
+        
+        output <- read.csv(output_file)
+        
+    }else{
+        
+        league_entries <- get_league_entries_updated(league_id) %>% rename(team_id = id)
+        
+        output <- 
+            do.call("rbind", lapply(league_entries$entry,
+                                    function(x){get_entry_player_picks(x, week_number) %>%
+                                            mutate(entry = x)})) %>%
+            inner_join(league_entries, by = "entry") %>%
+            mutate(league_id = league_id,
+                   week_number = week_number,
+                   league_name = get_league_updated(1138028)$league$name)
+        
+        write.csv(output,output_file)
+    }
+    
+    output
 }
