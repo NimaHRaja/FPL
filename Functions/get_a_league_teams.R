ab0d45adbb0faa112fa365e253a3905a317f0b8c
NimaHRaja <nimaradja@gmail.com>
2020-12-15 19:49:01 +0000

    refetch added to get_a_league_teams

--- Functions/get_a_league_teams.R
+++ Functions/get_a_league_teams.R
@@ -4,19 +4,25 @@
 # it also searches for a local version of the data file before 
 # fetching the data from FPL.
 
-get_a_league_teams <- function(league_id, week_number, first_page = 1, num_pages = 1){
+get_a_league_teams <- 
+    function(league_id, week_number, first_page = 1, num_pages = 1, refetch = FALSE){
     
     output_file <- 
         paste("data/entries_teams_", league_id, "_week", week_number, "_", 
               first_page, "-", num_pages, ".csv", sep = "")
     
-    if(file.exists(output_file)){
+    if(file.exists(output_file) & !refetch){
         
         output <- read.csv(output_file)
         
     }else{
         
-        league_entries <- get_league_entries_updated(league_id) %>% rename(team_id = id)
+        league_entries <- 
+            get_league_entries_updated(league_id, 
+                                       week_number = week_number,
+                                       first_page = first_page, 
+                                       num_pages = num_pages) %>% 
+            rename(team_id = id)
         league_name <- get_league_updated(league_id)$league$name
         
         output <- 
