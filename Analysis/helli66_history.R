b07c1b745cfd73880f5299ccdf1fabdb17e77ade
NimaHRaja <nimaradja@gmail.com>
2020-12-21 16:53:28 +0000

    graph added to AH66_history

--- Analysis/helli66_history.R
+++ Analysis/helli66_history.R
@@ -29,9 +29,18 @@ helli66_history_current <- rbind(helli66_history, helli66_current)
     # ggplot(aes(x = season, y = rank, colour = name)) + geom_point() + geom_line()
 
 
-jpeg("helli66_history.jpg", width = 1200, height = 600)
+# jpeg("helli66_history.jpg", width = 1200, height = 600)
 helli66_history %>%
     filter(season >= 2015) %>%
     ggplot(aes(x = season, y = total_points, colour = name)) + geom_point() + geom_line() +
     ylim(c(1500,NA))
+# dev.off()
+
+jpeg("Nima_kambiz_comparison.jpg", width = 1200, height = 600)
+helli66_history %>% 
+    filter(name %in% c("Kambiz Jamshidi", "Nima Hamedani-Raja")) %>% 
+    dcast(season ~ name, value.var = "total_points") %>%    
+    ggplot(aes(x =`Nima Hamedani-Raja`, y = `Kambiz Jamshidi`, label = season )) + 
+    geom_text(colour = "blue") +
+    geom_abline(slope = 1, intercept =  0, colour = "red")
 dev.off()
